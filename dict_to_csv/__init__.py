6c163a7c9c87dcb6353167bd7ff1af55fc7d0731
Thúlio Costa <95307+thulio@users.noreply.github.com>
2020-09-24 12:27:36 -0300

    feat: Support python 3.7+ (#1)

--- dict_to_csv/__init__.py
+++ dict_to_csv/__init__.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals, absolute_import
-
-import collections
+import collections.abc
 import functools
 import typing  # noqa: F401
 from contextlib import closing
@@ -10,39 +7,40 @@ from io import StringIO
 from dotmap import DotMap
 
 
-def empty_str(_):
-    # type: (typing.Any) -> typing.Text
+def empty_str(args: typing.Any) -> typing.Text:
     return ""
 
 
 DotMap.__str__ = empty_str
 
 
-def recursive_mapping_iterator(nested_mapping):
-    # type: (typing.Mapping) -> typing.Generator[typing.Tuple[typing.Text, typing.Any], None, None]  # noqa: E501
+def recursive_mapping_iterator(
+    nested_mapping: typing.Mapping,
+) -> typing.Generator[typing.Tuple[typing.Text, typing.Any], None, None]:  # noqa: E501
     for key, value in nested_mapping.items():
-        if isinstance(value, collections.Mapping):
+        if isinstance(value, collections.abc.Mapping):
             for inner_key, inner_value in recursive_mapping_iterator(value):
-                yield key + '.' + inner_key, inner_value
+                yield key + "." + inner_key, inner_value
         else:
             yield key, value
 
 
-def recursive_getattr(obj, attributes):
-    # type: (typing.Any, typing.Text) -> typing.Any
-    return functools.reduce(getattr, [obj] + attributes.split('.'))
+def recursive_getattr(obj: typing.Any, attributes: typing.Text) -> typing.Any:
+    return functools.reduce(getattr, [obj] + attributes.split("."))
 
 
-def nested_mapping_to_line(nested_mapping, keys):
-    # type: (typing.Mapping, typing.Sequence[typing.Text]) -> typing.Text
+def nested_mapping_to_line(
+    nested_mapping: typing.Mapping, keys: typing.Sequence[typing.Text]
+) -> typing.Text:
     dotted = DotMap(nested_mapping)
 
-    return ','.join([str(recursive_getattr(dotted, key)) for key in keys])
+    return ",".join([str(recursive_getattr(dotted, key)) for key in keys])
 
 
-def extract_header(data, stop_after=5):
-    # type: (typing.Sequence[typing.Mapping], int) -> typing.List[typing.Text]
-    keys = set()  # type: typing.Set[typing.Text]
+def extract_header(
+    data: typing.Sequence[typing.Mapping], stop_after: int = 5
+) -> typing.List[typing.Text]:
+    keys: typing.Set[typing.Text] = set()
 
     for nested_mapping in data:
         for key, value in recursive_mapping_iterator(DotMap(nested_mapping)):
@@ -63,17 +61,20 @@ def extract_header(data, stop_after=5):
     return sorted_keys
 
 
-def transform(data, include_headers=True, keys=None):
-    # type: (typing.Sequence[typing.Mapping], bool, typing.Sequence[typing.Text]) -> typing.Text # noqa: E501
+def transform(
+    data: typing.Sequence[typing.Mapping],
+    include_headers: bool = True,
+    keys: typing.Sequence[typing.Text] = None,
+):
     keys = keys or extract_header(data)
 
     with closing(StringIO()) as buff:
         if include_headers:
-            buff.write(','.join(keys))
-            buff.write('\n')
+            buff.write(",".join(keys))
+            buff.write("\n")
 
         for nested_mapping in data:
             buff.write(nested_mapping_to_line(nested_mapping, keys))
-            buff.write('\n')
+            buff.write("\n")
 
         return buff.getvalue()
